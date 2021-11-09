5010c6e479f41b00d6da0aff1f885058c8666fcc
lucakreativ <luca.kjell.e@gmail.com>
2021-11-09 07:44:23 +0100

    fixed sending Bug, now the variables get send and recieved

--- action_page.php
+++ action_page.php
@@ -3,10 +3,7 @@ $email = $_POST['Email'];
 $message = $_POST['Message'];
 
 
-$formcontent="From: $name \n Message: $message";
-$recipient = "email@email.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+echo $name;
+echo $email;
+echo $message;
 ?>
